0134eca8f29348016dc0aa617abe57190069d97b
hunglb <hunglb@techlab.ai>
2024-06-14 01:20:35 +0700

    init

---
+++ cards.js
@@ -0,0 +1,148 @@
+var errors = 0;
+var cardList = [
+    "darkness",
+    "double",
+    "fairy",
+    "fighting",
+    "fire",
+    "grass",
+    "lightning",
+    "metal",
+    "psychic",
+    "water"
+];
+
+var data = {
+    components: ['子', '女', '亻', '户', '氵', '火'],
+    correctCombinations: {
+        '女子': '好',
+        '亻子': '仔',
+        '氵户': '沪',
+        '火户': '炉'
+    },
+    selectedComponents: [],
+    result: ''
+}
+
+
+function selectComponent(event) {
+    const selected = event.currentTarget.dataset.component;
+    const {selectedComponents, correctCombinations} = this.data;
+
+    if (selectedComponents.length < 2) {
+        selectedComponents.push(selected);
+        this.setData({selectedComponents});
+    }
+
+    if (selectedComponents.length === 2) {
+        const combined = selectedComponents.join('');
+        const result = correctCombinations[combined] || '错误组合';
+        this.setData({result});
+        this.resetSelection();
+    }
+}
+
+function resetSelection() {
+    setTimeout(() => {
+        this.setData({selectedComponents: [], result: ''});
+    }, 1500);
+}
+
+
+var cardSet;
+var board = [];
+var rows = 4;
+var columns = 5;
+
+var card1Selected;
+var card2Selected;
+
+window.onload = function () {
+    shuffleCards();
+    startGame();
+}
+
+function shuffleCards() {
+    cardSet = cardList.concat(cardList); //two of each card
+    console.log(cardSet);
+    //shuffle
+    for (let i = 0; i < cardSet.length; i++) {
+        let j = Math.floor(Math.random() * cardSet.length); //get random index
+        //swap
+        let temp = cardSet[i];
+        cardSet[i] = cardSet[j];
+        cardSet[j] = temp;
+    }
+    console.log(cardSet);
+}
+
+function startGame() {
+    //arrange the board 4x5
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            let cardImg = cardSet.pop();
+            row.push(cardImg); //JS
+
+            // <img id="0-0" class="card" src="water.jpg">
+            let box = document.createElement("img");
+            box.id = r.toString() + "-" + c.toString();
+            box.src = cardImg + ".jpg";
+            box.classList.add("card");
+            box.addEventListener("click", selectCard);
+            document.getElementById("board").append(box);
+
+        }
+        board.push(row);
+    }
+
+    console.log(board);
+    setTimeout(hideCards, 1000);
+}
+
+function hideCards() {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let card = document.getElementById(r.toString() + "-" + c.toString());
+            card.src = "back.jpg";
+        }
+    }
+}
+
+function selectCard() {
+
+    if (this.src.includes("back")) {
+        if (!card1Selected) {
+            card1Selected = this;
+
+            let coords = card1Selected.id.split("-"); //"0-1" -> ["0", "1"]
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+
+            card1Selected.src = board[r][c] + ".jpg";
+        } else if (!card2Selected && this != card1Selected) {
+            card2Selected = this;
+
+            let coords = card2Selected.id.split("-"); //"0-1" -> ["0", "1"]
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+
+            card2Selected.src = board[r][c] + ".jpg";
+            setTimeout(update, 1000);
+        }
+    }
+
+}
+
+function update() {
+    //if cards aren't the same, flip both back
+    if (card1Selected.src != card2Selected.src) {
+        card1Selected.src = "back.jpg";
+        card2Selected.src = "back.jpg";
+        errors += 1;
+        document.getElementById("errors").innerText = errors;
+    }
+
+    card1Selected = null;
+    card2Selected = null;
+}
